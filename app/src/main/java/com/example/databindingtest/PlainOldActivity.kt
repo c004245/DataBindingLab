2150413a443a665bce73c39086a8a55d43b5f719
hyunwook <c004245@naver.com>
2019-08-21 17:01:44 +0900

    Activity xml

--- app/src/main/java/com/example/databindingtest/PlainOldActivity.kt
+++ app/src/main/java/com/example/databindingtest/PlainOldActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import com.example.databindingtest.data.SimpleViewModel
 import com.example.databindingtest.databinding.PlainActivityBinding
 
 class PlainOldActivity: AppCompatActivity() {
 
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(SimpleViewModel::class.java)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding : PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
 
-        binding.name = "CHO"
-        binding.lastName = "HYUNWOOK"
+        binding.lifecycleOwner = this
+
+        binding.viewmodel = viewModel
     }
 }
